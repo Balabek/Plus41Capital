5fc7c2bd2b8828adcdf047435e2def6ab3cc62fc
Balabek <vonbabayan@gmail.com>
2021-07-19 22:43:24 +0300

    Complete and responsive (EN/DE)

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    // Fancy animation for hamburger menu
+    $('.pluscapital_nav_toggler').on('click', function(){
+        $('.animated-icon1').toggleClass('open');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
